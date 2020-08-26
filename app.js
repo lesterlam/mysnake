f9426b09a8e8ec1b6ad0f3bf7c9dfbe8818e95ac
lesterlam <lesterlam@gmail.com>
2020-08-26 21:02:50 +0800

    Styles added

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const squares = document.querySelectorAll('.grid div')
-  const scoreDisplay = document.querySelector('span')
+  const scoreDisplay = document.querySelector('#score')
   const startBtn = document.querySelector('.start')
+  const pauseBtn = document.querySelector('.pause')
+  const resumeBtn = document.querySelector('.resume')
+  const restartBtn = document.querySelector('.restart')
 
   const width = 10
   let currentIndex = 0 //so first div in our grid
-  let appleIndex = 0 //so first div in our grid
-  let currentSnake = [2,1,0] // so the 3rd div in our grid being 2 (or the HEAD), and 0 being the end (TAIL, with all 1's being the body fro now on)
+  let heartIndex = 0 //so first div in our grid
+  let currentSnake = [2, 1, 0] // so the 3rd div in our grid being 2 (or the HEAD), and 0 being the end (TAIL, with all 1's being the body fro now on)
   let direction = 1
+  let headRotation = 'rotate(90deg)'
   let score = 0
   let speed = 0.9
   let intervalTime = 0
@@ -16,18 +20,59 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //to start, and restart the game
   function startGame() {
-    currentSnake.forEach(index => squares[index].classList.remove('snake'))
-    squares[appleIndex].classList.remove('apple')
+    squares.forEach(function(e) {
+      e.removeAttribute('class')
+    })
     clearInterval(interval)
     score = 0
-    randomApple()
+    randomheart()
     direction = 1
+    headRotation = 'rotate(90deg)'
     scoreDisplay.innerText = score
     intervalTime = 1000
-    currentSnake = [2,1,0]
+    currentSnake = [2, 1, 0]
     currentIndex = 0
     currentSnake.forEach(index => squares[index].classList.add('snake'))
+    squares[currentSnake[0]].classList.add('snakehead')
+    squares[currentSnake[0]].style.transform = headRotation
     interval = setInterval(moveOutcomes, intervalTime)
+
+    // Enable Control
+    enableControl()
+
+    // Change button to pause game
+    enableButton(pauseBtn)
+  }
+
+  // enable button
+  function enableButton(target) {
+    startBtn.style.display = 'none'
+    pauseBtn.style.display = 'none'
+    resumeBtn.style.display = 'none'
+    restartBtn.style.display = 'none'
+    target.style.display = 'inline-block'
+  }
+
+  //pause game
+  function pauseGame() {
+    clearInterval(interval)
+    disableControl()
+    enableButton(resumeBtn)
+  }
+
+  //resume game
+  function resumeGame() {
+    enableControl()
+    interval = setInterval(moveOutcomes, intervalTime)
+    enableButton(pauseBtn)
+  }
+
+  function disableControl() {
+    document.removeEventListener('keyup', control)
+  }
+
+  function enableControl() {
+    document.addEventListener('keyup', control)
   }
 
 
@@ -36,12 +81,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //deals with snake hitting border and snake hitting self
     if (
-      (currentSnake[0] + width >= (width * width) && direction === width ) || //if snake hits bottom
-      (currentSnake[0] % width === width -1 && direction === 1) || //if snake hits right wall
+      (currentSnake[0] + width >= (width * width) && direction === width) || //if snake hits bottom
+      (currentSnake[0] % width === width - 1 && direction === 1) || //if snake hits right wall
       (currentSnake[0] % width === 0 && direction === -1) || //if snake hits left wall
       (currentSnake[0] - width < 0 && direction === -width) ||  //if snake hits the top
       squares[currentSnake[0] + direction].classList.contains('snake') //if snake goes into itself
     ) {
+      disableControl()
+      enableButton(restartBtn)
       return clearInterval(interval) //this will clear the interval if any of the above happen
     }
 
@@ -49,12 +96,12 @@ document.addEventListener('DOMContentLoaded', () => {
     squares[tail].classList.remove('snake')  //removes class of snake from the TAIL
     currentSnake.unshift(currentSnake[0] + direction) //gives direction to the head of the array
 
-    //deals with snake getting apple
-    if(squares[currentSnake[0]].classList.contains('apple')) {
-      squares[currentSnake[0]].classList.remove('apple')
+    //deals with snake getting heart
+    if (squares[currentSnake[0]].classList.contains('heart')) {
+      squares[currentSnake[0]].classList.remove('heart')
       squares[tail].classList.add('snake')
       currentSnake.push(tail)
-      randomApple()
+      randomheart()
       score++
       scoreDisplay.textContent = score
       clearInterval(interval)
@@ -62,15 +109,19 @@ document.addEventListener('DOMContentLoaded', () => {
       interval = setInterval(moveOutcomes, intervalTime)
     }
     squares[currentSnake[0]].classList.add('snake')
+    squares[currentSnake[1]].classList.remove('snakehead')
+    squares[currentSnake[1]].style.transform = null
+    squares[currentSnake[0]].classList.add('snakehead')
+    squares[currentSnake[0]].style.transform = headRotation
   }
 
 
-  //generate new apple once apple is eaten
-  function randomApple() {
-    do{
-      appleIndex = Math.floor(Math.random() * squares.length)
-    } while(squares[appleIndex].classList.contains('snake')) //making sure apples dont appear on the snake
-    squares[appleIndex].classList.add('apple')
+  //generate new heart once heart is eaten
+  function randomheart() {
+    do {
+      heartIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[heartIndex].classList.contains('snake')) //making sure hearts dont appear on the snake
+    squares[heartIndex].classList.add('heart')
   }
 
 
@@ -78,17 +129,27 @@ document.addEventListener('DOMContentLoaded', () => {
   function control(e) {
     squares[currentIndex].classList.remove('snake') //we are removing the class of snake from ALL the squares.
 
-    if(e.keyCode === 39) {
+    if (e.keyCode === 39) {
       direction = 1 //if we press the right arrow on our keyboard, the snake will go right one
+      headRotation = 'rotate(90deg)'
     } else if (e.keyCode === 38) {
       direction = -width // if we press the up arrow, the snake will go back ten divs, appearing to go up
+      headRotation = 'rotate(0deg)'
     } else if (e.keyCode === 37) {
       direction = -1 // if we press left, the snake will go left one div
+      headRotation = 'rotate(-90deg)'
     } else if (e.keyCode === 40) {
       direction = +width //if we press down, the snake head will instantly appear in the div ten divs from where you are now
+      headRotation = 'rotate(180deg)'
     }
+    squares[currentSnake[0]].style.transform = headRotation
   }
 
-  document.addEventListener('keyup', control)
   startBtn.addEventListener('click', startGame)
+  pauseBtn.addEventListener('click', pauseGame)
+  resumeBtn.addEventListener('click', resumeGame)
+  restartBtn.addEventListener('click', startGame)
+
+  enableButton(startBtn)
+
 })
